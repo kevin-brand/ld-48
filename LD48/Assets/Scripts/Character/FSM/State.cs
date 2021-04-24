1facb593345ce74dae37a7a0071cc95396c8a660
Kevin Brand <kevin-brand@outlook.com>
2021-04-24 12:53:32 +0200

    added fsm classes

---
+++ LD48/Assets/Scripts/Character/FSM/State.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Character.FSM
+{
+    public class State
+    {
+        
+    }
+}
--- LD48/Assets/Scripts/Character/InputHandler.cs
+++ LD48/Assets/Scripts/Character/InputHandler.cs
@@ -5,9 +5,12 @@ namespace Character
 {
     public class InputHandler : MonoBehaviour
     {
+        private float _movementInput;
+        
         public void OnMoveInput(InputAction.CallbackContext context)
         {
-            
+            _movementInput = context.ReadValue<float>();
+            Debug.Log(_movementInput);
         }
 
         public void OnJumpInput(InputAction.CallbackContext context)
