b866461521975cd7370b247d46da2fd913385f92
Kevin Brand <kevin-brand@outlook.com>
2021-04-24 12:41:35 +0200

    added input handler class

---
+++ LD48/Assets/Scripts/Character/InputHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Character
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public void OnMoveInput(InputAction.CallbackContext context)
+        {
+            
+        }
+
+        public void OnJumpInput(InputAction.CallbackContext context)
+        {
+            
+        }
+    }
+}
