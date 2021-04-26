87ff12bd7ff3c6ad159be4e2971bcd1bafd1d896
Kevin Brand <kevin-brand@outlook.com>
2021-04-26 11:42:59 +0200

    started to work on pickup display

--- LD48/Assets/Scripts/Bombs/BombPickup.cs
+++ LD48/Assets/Scripts/Bombs/BombPickup.cs
@@ -8,14 +8,18 @@ using UnityEngine.InputSystem;
 public class BombPickup : MonoBehaviour
 {
     [SerializeField] private BombData bomb;
-
+    [SerializeField] public GameObject warningEffect;
+    
     private bool _playerIsInTriggerRange = false;
     private BombBag _bag;
 
+    private List<GameObject> _warningEffects = new List<GameObject>();
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<BombBag>())
         {
+            DisplayWarningEffect();
             _playerIsInTriggerRange = true;
             _bag = other.GetComponent<BombBag>();
         }
@@ -25,9 +29,39 @@ public class BombPickup : MonoBehaviour
     {
         if (other.GetComponent<BombBag>())
         {
+            RemoveWarningEffect();
             _playerIsInTriggerRange = false;
             _bag = null;
         }
+    }
+    
+    private void DisplayWarningEffect()
+    {
+        if (warningEffect == null)
+            return;
+
+        List<Vector2> positions = bomb.pattern.GetExplosionPositions(transform.position);
+            
+        foreach (var detonationPosition in positions)
+        {
+            Debug.Log(detonationPosition.ToString());
+            
+            Vector3 detonationWorldPosition = new Vector3(detonationPosition.x, detonationPosition.y, 0);
+            GameObject go = Instantiate(warningEffect, detonationWorldPosition, Quaternion.identity, this.transform) as GameObject;
+            _warningEffects.Add(go);
+        }
+    }
+    
+    private void RemoveWarningEffect()
+    {
+        if (warningEffect == null)
+            return;
+            
+        foreach (var warning in _warningEffects)
+        {
+            Destroy(warning);
+        }
+        _warningEffects.Clear();
     }
     
     public void OnBombOneInput(InputAction.CallbackContext context)
