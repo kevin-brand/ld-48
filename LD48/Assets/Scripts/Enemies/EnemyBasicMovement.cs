504f5fc8c675e5cc67f5fdbb9b85b7ebf9c9c1be
Julian Vu <julian@vu-online.de>
2021-04-26 15:09:06 +0200

    made many different slimes

--- LD48/Assets/Scripts/Enemies/EnemyBasicMovement.cs
+++ LD48/Assets/Scripts/Enemies/EnemyBasicMovement.cs
@@ -39,7 +39,7 @@ namespace Enemies
                 Dir *= -1;
             }
             Rigidbody2D.velocity = new Vector2(Dir * speed, Rigidbody2D.velocity.y);
-            transform.localScale = new Vector2(Dir * .7f, .7f);
+            transform.localScale = new Vector2(Dir * transform.localScale.x, transform.localScale.y);
         }
     }
 }
