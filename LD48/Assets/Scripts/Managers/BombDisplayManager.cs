bcdc4ac3d4e1cd1e65fbc9954d8cf01b13f7cf4a
Kevin Brand <kevin-brand@outlook.com>
2021-04-26 13:37:26 +0200

    added bomb display number

--- LD48/Assets/Scripts/Managers/BombDisplayManager.cs
+++ LD48/Assets/Scripts/Managers/BombDisplayManager.cs
@@ -11,6 +11,9 @@ namespace Managers
         public TMP_Text bombOneNameText;
         public TMP_Text bombTwoNameText;
         
+        public TMP_Text bombOneCounterText;
+        public TMP_Text bombTwoCounterText;
+        
         private void Start()
         {
             if (bombBag == null)
@@ -21,6 +24,9 @@ namespace Managers
         {
             bombOneNameText.text = bombBag.GetSlot(0).Bomb.bombName;
             bombTwoNameText.text = bombBag.GetSlot(1).Bomb.bombName;
+
+            bombOneCounterText.text = bombBag.GetSlot(0).held.ToString();
+            bombTwoCounterText.text = bombBag.GetSlot(1).held.ToString();
         }
     }
 }
