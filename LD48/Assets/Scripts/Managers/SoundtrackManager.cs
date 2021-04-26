b8d364c83c181acc7da7e5846aa16e475c557904
Julian Vu <julian@vu-online.de>
2021-04-26 14:49:06 +0200

    fixed UI and Sound Manager

--- LD48/Assets/Scripts/Managers/SoundtrackManager.cs
+++ LD48/Assets/Scripts/Managers/SoundtrackManager.cs
@@ -26,36 +26,42 @@ namespace Managers
             if (_source.isPlaying == false)
             {
                 int r = Random.Range(0, 2);
-            
+                Debug.Log(r);
                 switch (_lastAudioIndex)
                 {
                     case 0:
                         _source.clip = A1;
                         _source.Play();
+                        _lastAudioIndex = 1;
                         break;
                     case 1:
                         if (ScoreManager.Instance.GetDepthScore() >= 50)
                         {
-                            r = 2;
+                            r = 2;                            
                         }
                         _source.clip = (r == 0) ? A1 : (r == 1) ? A2 : AB;
                         _source.Play();
+                        _lastAudioIndex = r + 1;
                         break;
                     case 2:
                         _source.clip = A1;
                         _source.Play();
+                        _lastAudioIndex = 1;
                         break;
                     case 3:
                         _source.clip = B1;
                         _source.Play();
+                        _lastAudioIndex = 4;
                         break;
                     case 4:
                         _source.clip = (r == 0) ? B1 : B2;
                         _source.Play();
+                        _lastAudioIndex = r + 4;
                         break;
                     case 5:
                         _source.clip = (r == 0) ? B1 : B2;
                         _source.Play();
+                        _lastAudioIndex = r + 4;
                         break;
                     default:
                         break;
